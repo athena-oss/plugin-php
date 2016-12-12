a59514330172b9e4a5487d06507ae2d7e52e2c0f
Caio Moritz Ronchi <caiomoritz@gmail.com>
2016-12-12 15:52:50 +0100

    Restored BrowserDriverBuilder::fromSettings (fixes #10)

---
+++ src/Browser/AthenaBrowserDriverBuilder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Athena\Browser;
+
+use Athena\Configuration\Settings;
+use OLX\FluentWebDriverClient\Browser\BrowserDriverBuilder;
+
+class AthenaBrowserDriverBuilder
+{
+    /**
+     * @param \Athena\Configuration\Settings $settings
+     *
+     * @return BrowserDriverBuilder
+     * @throws \Athena\Exception\SettingNotFoundException
+     */
+    public static function fromAthenaSettings(Settings $settings)
+    {
+        $seleniumHubUrl  = $settings->getByPath('selenium.hub_url')->orFail();
+        $implicitTimeout = $settings->getByPath('selenium.implicit_timeout')->orDefaultTo(0);
+        $connectionTimeout = $settings->getByPath('selenium.connection_timeout')->orDefaultTo(null);
+        $requestTimeout = $settings->getByPath('selenium.request_timeout')->orDefaultTo(null);
+        $extraCapabilities = $settings->getByPath('selenium.browser.capabilities')->orDefaultTo([]);
+        $builder = (new BrowserDriverBuilder($seleniumHubUrl))
+            ->withType($settings->get('browser')->orFail())
+            ->withProxySettings($settings->get('proxy')->orDefaultTo([]))
+            ->withImplicitTimeout($implicitTimeout)
+            ->withConnectionTimeout($connectionTimeout)
+            ->withRequestTimeout($requestTimeout)
+            ->withExtraCapabilities($extraCapabilities)
+            ->withUrls($settings->get('urls')->orDefaultTo([]));
+        return $builder;
+    }
+}
